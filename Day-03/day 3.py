dd31a27502bc3c93004a94e828bef136dccc357f
VarunRaj-16 <varunrajvarakala16@gmail.com>
2026-09-15 15:47:46 +0530

    Identifiers,Literals

---
+++ Day-03/day 3.py
@@ -0,0 +1,34 @@
+#Identifiers
+student_name = "Varun"
+print(student_name)
+
+#Literals
+age = 24
+name = "Varun"
+isPassed = True
+
+#Operators
+a = 10
+b = 20
+print(a + b)     # Output: 30
+
+#Statements
+print("Hello")
+a = 10
+b = 20
+
+#Variable
+a = 10
+print(a)         # Output: 10
+
+#Multiple Assignment
+a, b, c = 10, 20, 30
+print(a)
+print(b)
+print(c)
+
+#Reassignment
+a = 10
+print(a)
+a = 100
+print(a)
